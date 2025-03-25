2d106e99050ecfefcb7799cf6c72b614e915b560
jonoya <jonoyanguren@gmail.com>
2025-03-25 11:47:27 +0100

    feat: fix pricing in home and languages

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,7 +8,8 @@ import Benefits from "@/components/Benefits";
 import { OrangeMessage } from "@/components/OrangeMessage";
 import { Testimonials } from "@/components/Testimonials";
 import { AboutTheDesigner } from "@/components/AboutTheDesigner";
-import { Pricing } from "@/components/Pricing";
+import PricingList from "@/components/PricingList";
+import Image from "next/image";
 
 export default function HomePage() {
   const t = useTranslations("home");
@@ -31,7 +32,23 @@ export default function HomePage() {
           <Steps />
         </div>
         <Benefits />
-        <Pricing />
+        <div className="relative flex flex-col md:flex-row items-center mt-12 md:mt-0 mx-4 md:mx-16">
+          <Image
+            className="absolute -top-8 md:top-20 md:-left-20 md:mb-0"
+            src="/halfOrange.svg"
+            alt="halfOrange"
+            width={100}
+            height={100}
+          />
+          <Title
+            title={t("pricing.title")}
+            description={t("pricing.description")}
+          />
+        </div>
+
+        <div className="flex flex-col md:flex-row mx-4 mt-4 md:mt-0 md:mx-16 mb-16">
+          <PricingList />
+        </div>
         <AboutTheDesigner />
         <OrangeMessage message={t("orangeMessage")} highlightIndexes={[5, 6]} />
 
